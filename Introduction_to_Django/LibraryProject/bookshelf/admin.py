a52b15e3edb2264e5eb5a56f071d726bc8cc7bba
FawazofLagos <aybest12452gmail.com>
2025-11-02 01:48:40 -0600

    Registereed Book Model and customized Django admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year') #show these columns
+    search_fields = ('title', 'author') #add search functionality
+    list_filter = ('publication_year',) #add filter by publication year
