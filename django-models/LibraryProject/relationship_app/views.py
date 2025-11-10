044a2bfe82fbf9328323e637bca91c018b720553
FawazofLagos <aybest12452gmail.com>
2025-11-09 20:00:40 -0600

    Add function-based and class-based views with URL configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+def list_books(request):
+    """Display a list of all books and their authors."""
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    """Display details of a specific library, including its books."""
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 # Create your views here.
