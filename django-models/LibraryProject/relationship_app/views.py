ce9773ddd2661771e29f2f6f055ee26b0171bcad
FawazofLagos <aybest12452gmail.com>
2025-11-16 16:10:25 -0600

    fixed checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
-from .models import Book, Library, UserProfile
+from .models import Book
+from .models import Library
 
 # ---------------------------
 # Book and Library Views
