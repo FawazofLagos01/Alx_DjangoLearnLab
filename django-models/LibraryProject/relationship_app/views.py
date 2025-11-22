aa776bc3cd8b42a2bb5e0a59eebb1c22f3c50128
FawazofLagos <aybest12452gmail.com>
2025-11-22 16:35:31 -0600

    added the missing code block

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
-from .models import Book, Library
-
+from .models import Book
+from .models import Library
 
 # ---------------------------
 # Book and Library Views
