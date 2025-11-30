2aed2b908ebfb5e31bbe374ea07b34ce1a58cb3e
FawazofLagos <aybest12452gmail.com>
2025-11-29 22:41:34 -0600

    adding custom serializers in new DRF project

---
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class AuthorListCreateView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
