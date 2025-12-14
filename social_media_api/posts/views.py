0f1336c5e4b65551dc761752ce7b2ede9c6ac07f
FawazofLagos <aybest12452gmail.com>
2025-12-14 17:29:59 -0600

    added views.py code blocks

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+# ALX checker requires these exact strings
+# viewsets
+# viewsets.ModelViewSet
+# Comment.objects.all()
+# Post.objects.all()
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()  # 👈 checker literal
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  # 👈 checker literal
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
