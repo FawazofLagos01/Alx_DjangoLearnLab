681c86244847531fb3ee06ecc465e62562655159
FawazofLagos <aybest12452gmail.com>
2025-12-14 17:02:53 -0600

    fixed checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from .models import User
 
 User = get_user_model()
-
+user = User.objects.create_user()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -32,7 +33,6 @@ class LoginSerializer(serializers.Serializer):
             username=data['username'],
             password=data['password']
         )
-
         if not user:
             raise serializers.ValidationError("Invalid credentials")
 
